aff7cc31ff5d893334cb0fc030d2298cbdce5d4e
lbt05 <lbt0506@gmail.com>
2016-07-04 19:14:11 +0800

    fix bugs in response filter

--- lib/src/main/java/cn/leancloud/ResponseUtil.java
+++ lib/src/main/java/cn/leancloud/ResponseUtil.java
@@ -1,5 +1,6 @@
 package cn.leancloud;
 
+
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
@@ -10,7 +11,7 @@ public class ResponseUtil {
 
   public static String filterResponse(String response) {
     JSONObject resp = JSON.parseObject(response, JSONObject.class);
-    Object result = resp.get("results");
+    Object result = resp.get("result");
     if (result instanceof JSONObject) {
       removeType((JSONObject) result);
     } else if (result instanceof JSONArray) {
@@ -21,10 +22,11 @@ public class ResponseUtil {
       }
     }
     return AVUtils.restfulCloudData(resp);
-  } 
+  }
 
   private static void removeType(JSONObject object) {
     if (object.containsKey("className") && object.containsKey(TYPE)) {
+      object.remove("className");
       object.remove(TYPE);
     }
   }
