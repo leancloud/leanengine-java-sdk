0dc2adff957c9e2256239d05f503e3b440156a97
lbt05 <lbt0506@gmail.com>
2016-07-05 18:18:03 +0800

    change params matching seq

--- lib/src/main/java/cn/leancloud/EngineFunctionHandlerInfo.java
+++ lib/src/main/java/cn/leancloud/EngineFunctionHandlerInfo.java
@@ -14,28 +14,38 @@ public class EngineFunctionHandlerInfo extends EngineHandlerInfo {
   }
 
   @Override
-  public Object parseParams(String requestBody) {
-    switch (methodParameterList.size()) {
-      case 0:
-        return null;
-      case 1:
-        Object param = null;
-        try {
-          EngineFunctionParamInfo paramInfo = methodParameterList.get(0);
-          param = paramInfo.parseParams(requestBody);
-          // 这里是假设唯一的目标作为参数传递过来，而不是放在一个jsonObject中间
-          return param;
-        } catch (Exception e) {
-          // 如果解析出错了，就认为传递过来的是一个jsonObject可以按照多于1个参数的情况来解析
-        }
-      default:
-        Map jsonParams = JSON.parseObject(requestBody, Map.class);
-        Object[] params = new Object[methodParameterList.size()];
+  public Object parseParams(String requestBody) throws InvalidParameterException {
+    if (methodParameterList.size() == 0) {
+      return null;
+    } else {
+      Map jsonParams = JSON.parseObject(requestBody, Map.class);
+      Object[] params = new Object[methodParameterList.size()];
+      try {
         for (int index = 0; index < methodParameterList.size(); index++) {
           Object p = jsonParams.get(methodParameterList.get(index).name);
+          if (p == null) {
+            throw new InvalidParameterException();
+          }
           params[index] = methodParameterList.get(index).parseParams(JSON.toJSONString(p));
         }
-        return params;
+      } catch (InvalidParameterException e) {
+        if (methodParameterList.size() == 1) {
+          // when
+          Object param = null;
+          try {
+            EngineFunctionParamInfo paramInfo = methodParameterList.get(0);
+            param = paramInfo.parseParams(requestBody);
+            // 这里是假设唯一的目标作为参数传递过来，而不是放在一个jsonObject中间
+            return param;
+          } catch (Exception e1) {
+            throw new InvalidParameterException();
+            // 如果解析出错了，就认为传递过来的是一个jsonObject可以按照多于1个参数的情况来解析
+          }
+        } else {
+          throw e;
+        }
+      }
+      return params;
     }
   }
 }
