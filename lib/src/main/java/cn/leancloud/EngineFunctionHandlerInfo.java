3bf5eac206dc129c4f144fb75c1f2deeb0c59f14
lbt05 <lbt0506@gmail.com>
2016-07-07 16:55:03 +0800

    catch json parse

--- lib/src/main/java/cn/leancloud/EngineFunctionHandlerInfo.java
+++ lib/src/main/java/cn/leancloud/EngineFunctionHandlerInfo.java
@@ -18,9 +18,9 @@ public class EngineFunctionHandlerInfo extends EngineHandlerInfo {
     if (methodParameterList.size() == 0) {
       return null;
     } else {
-      Map jsonParams = JSON.parseObject(requestBody, Map.class);
-      Object[] params = new Object[methodParameterList.size()];
       try {
+        Map jsonParams = JSON.parseObject(requestBody, Map.class);
+        Object[] params = new Object[methodParameterList.size()];
         for (int index = 0; index < methodParameterList.size(); index++) {
           Object p = jsonParams.get(methodParameterList.get(index).name);
           if (p == null) {
