f231926fea5fed75003da877a0ca073ce6975734
lbt05 <lbt0506@gmail.com>
2016-07-07 16:54:49 +0800

    rm useless import

--- lib/src/main/java/cn/leancloud/LeanEngineHealthCheckServlet.java
+++ lib/src/main/java/cn/leancloud/LeanEngineHealthCheckServlet.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.alibaba.fastjson.JSONObject;
-import com.avos.avoscloud.AVUtils;
 import com.avos.avoscloud.PaasClient;
 
 /**
