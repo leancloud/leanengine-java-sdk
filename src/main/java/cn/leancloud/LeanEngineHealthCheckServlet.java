386a76f3685e3bad9e97d45058f9b3a8be180ccd
lbt05 <lbt0506@gmail.com>
2016-08-09 17:46:53 +0800

    update pom

--- src/main/java/cn/leancloud/LeanEngineHealthCheckServlet.java
+++ src/main/java/cn/leancloud/LeanEngineHealthCheckServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.alibaba.fastjson.JSONObject;
-import com.avos.avoscloud.PaasClient;
+import com.avos.avoscloud.internal.InternalConfigurationController;
 
 /**
  * 定义云函数中的健康检查函数
@@ -23,12 +23,13 @@ public class LeanEngineHealthCheckServlet extends HttpServlet {
   private static final long serialVersionUID = -7406297470714318279L;
 
   @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+      IOException {
     resp.setHeader("content-type", LeanEngine.JSON_CONTENT_TYPE);
     JSONObject result = new JSONObject();
     result.put("runtime", System.getProperty("java.version"));
-    result.put("version", PaasClient.sdkVersion);
+    result.put("version", InternalConfigurationController.globalInstance().getClientConfiguration()
+        .getUserAgent());
     resp.getWriter().write(result.toJSONString());
   }
 }
